1e7dbbe4933b31dbb8b04ad07cd3c3038986dcc9
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 16:47:46 -0300

    Add files via upload

---
+++ atividade-tema12-3.py
@@ -0,0 +1,7 @@
+# Inicializa o número atual como 101, o primeiro número ímpar após 100
+numero_atual = 101
+
+# Usa um laço while para gerar e imprimir números ímpares até 200
+while numero_atual <= 200:
+    print(numero_atual)
+    numero_atual += 2  # Incrementa o número atual em 2 para obter o próximo número ímpar
